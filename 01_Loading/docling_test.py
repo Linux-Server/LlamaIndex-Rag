3c826564de04b8c93401f8c870208ec33430962a
Sachin <rollercoster246@gmail.com>
2026-07-22 18:53:41 +0530

    loading

--- 01_Loading/docling_test.py
+++ 01_Loading/docling_test.py
@@ -1,9 +1,15 @@
-from docling.document_converter import DocumentConverter
 
-source_file = "https://arxiv.org/pdf/1706.03762"
+source = "/Users/sachin/Desktop/LlamaIndex-Rag/docs"  # file path or URL
+from llama_index.core import SimpleDirectoryReader
 
-converter = DocumentConverter()
+documents = SimpleDirectoryReader(source).load_data()
 
-doc = converter.convert(source_file).document
 
-print(doc)
+from llama_index.core.node_parser import MarkdownNodeParser
+
+parser = MarkdownNodeParser()
+
+nodes = parser.get_nodes_from_documents(documents)
+
+
+print(nodes[3].metadata)
